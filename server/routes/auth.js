d8ebdca5a5489ed5f759bc2209162917af9f0808
Jesudas Joseph <jesudasmjoseph@gmail.com>
2020-12-15 15:37:58 -0800

    Tracks IP address more accurately (IPv6 prefix needs to be removed)

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'application/json');
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	token = auth.authenticate("jess", req.headers.origin);
+	token = auth.authenticate("jess", req.ip);
 	res.send(JSON.stringify({status: "0", token: token}));
 
 });
