2579f446e9f060937385cf7239ab13c41780b294
Jesudas Joseph <jesudasmjoseph@gmail.com>
2020-12-18 23:34:40 -0800

    Changed 'auth' to 'authenticator' in auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-let auth = require('../authenticator');
+let authenticator = require('../authenticator');
 
 /*GET request for /auth (returns token)*/
 router.get('/', function(req, res, next) {
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'application/json');
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	token = auth.authenticate("jess", req.ip);
+	token = authenticator.authenticate("jess", req.ip);
 	res.send(JSON.stringify({status: "0", token: token}));
 
 });
