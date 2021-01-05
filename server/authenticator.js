44d19491fc2851eda27fc3f603eb06242b41510a
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-01-04 23:22:49 -0800

    added crypto and webcrypto to package.json

--- server/authenticator.js
+++ server/authenticator.js
@@ -1,17 +1,33 @@
+//Not sure if this is depricated
+const crypto = require('crypto');
 let current_users = [];
 let next_token = 1111111111111111;
 let last_token;
 
-function authenticate(user, ip){
-	last_token = next_token;
-	next_token++;
-	current_users.push({"user":user, "ip":ip, "token":last_token});
-	console.log(current_users);
-	return last_token;
+function generateToken(username, ip){
+	return crypto.webcrypto.generateKey({name:"AES-CBC", length:256}, false, ["verify"], (err, key) => {
+		if (err) throw err;
+	});
 }
 
-function checkAuthentication(){
+function getToken(username, password, ip){
+	//Add password encryption
+	//Checks username and password match
+	if (username == "jess" && password == "password"){
+		return generateToken(username, ip);
+	}
+	else{
+		//Failed to getToken
+		//Password and Username do not match
+		return -1;
+	}
+}
 
+function authenticate(user, ip){
+	const token = getToken("jess", "password", ip);
+	current_users.push({"user":user, "ip":ip, "token":token});
+	console.log(current_users);
+	return token;
 }
 
 function getCurrentUsers(){
