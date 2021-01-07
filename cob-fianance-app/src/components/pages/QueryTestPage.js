169966546e64b081553c24333387e3e50a5454d1
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-01-07 14:51:37 -0800

    Added test code to QueryTestPage

--- cob-fianance-app/src/components/pages/QueryTestPage.js
+++ cob-fianance-app/src/components/pages/QueryTestPage.js
@@ -1,59 +1,74 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 
-export default function Login() {
+export default class QueryTestPage extends React.Component {
 
-	//async function postData(url = '', data = {}) {
-	//  // Default options are marked with *
-	//  const response = await fetch(url, {
-	//	method: 'POST', // *GET, POST, PUT, DELETE, etc.
-	//	mode: 'no-cors', // no-cors, *cors, same-origin
-	//	cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-	//	credentials: 'same-origin', // include, *same-origin, omit
-	//	headers: {
-	//	  'Content-Type': 'application/json'
-	//	  // 'Content-Type': 'application/x-www-form-urlencoded',
-	//	},
-	//	redirect: 'follow', // manual, *follow, error
-	//	referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-	//	body: JSON.stringify(data) // body data type must match "Content-Type" header
-	//  });
-	//  console.log(response);
-	//  return response; // parses JSON response into native JavaScript objects
-  	//}
+	constructor(props) {
+	    super(props);
+	    this.state = {token: "meh"};
+		this.postDataTest = this.postDataTest.bind(this);
+		this.getDataTest = this.getDataTest.bind(this);
+	}
 
-	function postDataTest() {
-	//	  let tempdata = postData('http://localhost:2700', { requestType: "login", username: "username", password: "password" })
- 	//	  .then(data => {
-	//		console.log("here");
- 	// 		console.log(data); // JSON data parsed by `data.json()` call
- 	// 	  });
-	//	  console.log("here!");
+	postDataTest() {
 
-	fetch('http://localhost:2700/q?' + "q=login&username=username&password=password", {
-		mode: 'cors',
-	})
-	.then(response => {
-		console.log(response);
-		return response.json();
-	})
-	.then(data => {
-		console.log('Success:', data);
-	})
-	.catch((error) => {
-		console.error('Error:', error);
-	});
-	  }
+		fetch('http://localhost:2700/auth', {
+			mode: 'cors',
+			method: 'GET',
+			credentials: 'same-origin',
+		}).then(response => {
+			return response.json();
+		}).then(data => {
+			this.setState({token: data.token});
+			console.log('Success:', data);
+		}).catch((error) => {
+			console.error('Error:', error);
+		});
 
-    return (
-        <React.Fragment >
-            <div style={{flex: '1', paddingTop:'5px', paddingBottom:'5px' , textAlign: 'center' }}><>
-            <Button onClick={postDataTest} style={{flex: '1' , textAlign: 'center', width:'700px',   display: 'inline-block', height: '100px' }} as="input" type="button" value="Get Token" />{' '}</>
-            </div>
-            <div style={{ flex: '1', padding: '5px 5px' , textAlign: 'center'}}><>
-            <Button style={{flex: '1' , textAlign: 'center', width:'700px',   display: 'inline-block', height: '100px' }} as="input" type="button" value="Get User Transactions" />{' '}</>
-            </div>
 
-        </React.Fragment>
-    )
+
+	}
+
+
+	getDataTest() {
+		//	  let tempdata = postData('http://localhost:2700', { requestType: "login", username: "username", password: "password" })
+		//	  .then(data => {
+		//		console.log("here");
+		// 		console.log(data); // JSON data parsed by `data.json()` call
+		// 	  });
+		//	  console.log("here!");
+
+		fetch('http://localhost:2700/deposit', {
+			mode: 'cors',
+			method: 'GET',
+			credentials: 'same-origin',
+			headers: {
+				'Authorization': '{"token":"548148593027jess::ffff:127.0.0.1", "username":"jess"}'
+			},
+		})
+		.then(response => {
+			console.log(response);
+			return response.text();
+		})
+		.then(data => {
+			console.log('Success:', data);
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+		});
+	}
+
+	render () {
+	    return (
+	        <React.Fragment >
+	            <div style={{flex: '1', paddingTop:'5px', paddingBottom:'5px' , textAlign: 'center' }}><>
+	            <Button onClick={this.postDataTest} style={{flex: '1' , textAlign: 'center', width:'700px',   display: 'inline-block', height: '100px' }} as="input" type="button" value={this.state.token} />{' '}</>
+	            </div>
+	            <div style={{ flex: '1', padding: '5px 5px' , textAlign: 'center'}}><>
+	            <Button style={{flex: '1' , textAlign: 'center', width:'700px',   display: 'inline-block', height: '100px' }} as="input" type="button" value="Get User Transactions" />{' '}</>
+	            </div>
+
+	        </React.Fragment>
+	    );
+	}
 }
