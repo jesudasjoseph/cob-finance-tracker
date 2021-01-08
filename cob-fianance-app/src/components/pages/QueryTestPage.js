b11cb95bfab2342549d0e91cd125943943bfb21e
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-01-07 21:40:06 -0800

    Added many buttons and structure for future tests.

--- cob-fianance-app/src/components/pages/QueryTestPage.js
+++ cob-fianance-app/src/components/pages/QueryTestPage.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 
+let token = 0;
+
 export default class QueryTestPage extends React.Component {
 
 	constructor(props) {
 	    super(props);
-	    this.state = {token: "meh"};
-		this.postDataTest = this.postDataTest.bind(this);
-		this.getDataTest = this.getDataTest.bind(this);
+	    this.state = {
+			button0Text: "Get Token!",
+			button1Text: "Validate Token!",
+			button2Text: "Text",
+			button3Text: "Text",
+			button4Text: "Text",
+			button5Text: "Text"
+		};
+
+		this.getToken = this.getToken.bind(this);
+		this.checkToken = this.checkToken.bind(this);
 	}
 
-	postDataTest() {
+	getToken() {
 
 		fetch('http://localhost:2700/auth', {
 			mode: 'cors',
@@ -19,31 +29,26 @@ export default class QueryTestPage extends React.Component {
 		}).then(response => {
 			return response.json();
 		}).then(data => {
-			this.setState({token: data.token});
+			if (data.token){
+				token = data.token;
+				this.setState({button0Text: token});
+			} else {
+				this.setState({button0Text: "Failed to get Token!"});
+				token = 0;
+			}
 			console.log('Success:', data);
 		}).catch((error) => {
 			console.error('Error:', error);
 		});
-
-
-
 	}
 
-
-	getDataTest() {
-		//	  let tempdata = postData('http://localhost:2700', { requestType: "login", username: "username", password: "password" })
-		//	  .then(data => {
-		//		console.log("here");
-		// 		console.log(data); // JSON data parsed by `data.json()` call
-		// 	  });
-		//	  console.log("here!");
-
+	checkToken() {
 		fetch('http://localhost:2700/deposit', {
 			mode: 'cors',
 			method: 'GET',
 			credentials: 'same-origin',
 			headers: {
-				'Authorization': '{"token":"548148593027jess::ffff:127.0.0.1", "username":"jess"}'
+				'Authorization': `{"token":"${token}", "user":"jess"}`
 			},
 		})
 		.then(response => {
@@ -51,6 +56,7 @@ export default class QueryTestPage extends React.Component {
 			return response.text();
 		})
 		.then(data => {
+			this.setState({button1Text: data});
 			console.log('Success:', data);
 		})
 		.catch((error) => {
@@ -62,10 +68,22 @@ export default class QueryTestPage extends React.Component {
 	    return (
 	        <React.Fragment >
 	            <div style={{flex: '1', paddingTop:'5px', paddingBottom:'5px' , textAlign: 'center' }}><>
-	            <Button onClick={this.postDataTest} style={{flex: '1' , textAlign: 'center', width:'700px',   display: 'inline-block', height: '100px' }} as="input" type="button" value={this.state.token} />{' '}</>
+	            <Button onClick={this.getToken} style={{flex: '1' , textAlign: 'center', width:'700px',   display: 'inline-block', height: '50px' }} as="input" type="button" value={this.state.button0Text} />{' '}</>
 	            </div>
 	            <div style={{ flex: '1', padding: '5px 5px' , textAlign: 'center'}}><>
-	            <Button style={{flex: '1' , textAlign: 'center', width:'700px',   display: 'inline-block', height: '100px' }} as="input" type="button" value="Get User Transactions" />{' '}</>
+	            <Button onClick={this.checkToken} style={{flex: '1' , textAlign: 'center', width:'700px',   display: 'inline-block', height: '50px' }} as="input" type="button" value={this.state.button1Text} />{' '}</>
+	            </div>
+				<div style={{ flex: '1', padding: '5px 5px' , textAlign: 'center'}}><>
+	            <Button style={{flex: '1' , textAlign: 'center', width:'700px',   display: 'inline-block', height: '50px' }} as="input" type="button" value={this.state.button2Text} />{' '}</>
+	            </div>
+				<div style={{ flex: '1', padding: '5px 5px' , textAlign: 'center'}}><>
+	            <Button style={{flex: '1' , textAlign: 'center', width:'700px',   display: 'inline-block', height: '50px' }} as="input" type="button" value={this.state.button3Text} />{' '}</>
+	            </div>
+				<div style={{ flex: '1', padding: '5px 5px' , textAlign: 'center'}}><>
+	            <Button style={{flex: '1' , textAlign: 'center', width:'700px',   display: 'inline-block', height: '50px' }} as="input" type="button" value={this.state.button4Text} />{' '}</>
+	            </div>
+				<div style={{ flex: '1', padding: '5px 5px' , textAlign: 'center'}}><>
+	            <Button style={{flex: '1' , textAlign: 'center', width:'700px',   display: 'inline-block', height: '50px' }} as="input" type="button" value={this.state.button5Text} />{' '}</>
 	            </div>
 
 	        </React.Fragment>
