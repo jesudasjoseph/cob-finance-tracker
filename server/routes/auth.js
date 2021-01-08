2109aff221097f2749df1473701a34c932d18dd6
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-01-07 21:39:22 -0800

    Send '0' when token already exists.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,12 +4,16 @@ let authenticator = require('../authenticator');
 
 /*GET request for /auth (returns token)*/
 router.get('/', function(req, res, next) {
-
-	res.statusCode = 200;
+	let token;
+	//res.statusCode = 200;
 	res.setHeader('Content-Type', 'application/json');
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	token = authenticator.authenticate("jess", req.ip);
-	res.send(JSON.stringify({status: "0", token: token}));
+	try {
+		token = authenticator.authenticate("jess", "password", req.ip);
+	}
+	catch(error){
+		token = 0;
+	}
+	res.send(JSON.stringify({token: token}));
 
 });
 
