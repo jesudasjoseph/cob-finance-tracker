f1a4658b9c1d01f7d1a48cec958896d799706a96
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-01-07 21:38:38 -0800

    Added session object constructor.

--- server/authenticator.js
+++ server/authenticator.js
@@ -1,4 +1,14 @@
-let current_users = [];
+const SESSION_TIMEOUT = 5000; //seconds
+
+function session(user, ip, token) {
+	this.user = user;
+	this.ip = ip;
+	this.token = token;
+	this.lifespan = SESSION_TIMEOUT;
+	this.timeout = SESSION_TIMEOUT;
+}
+
+let sessionList = [];
 
 //Insecure! Replace with a proper crypto safe random key generator or a hash function that takes username+ip+salt
 function generateToken(username, ip){
@@ -6,13 +16,16 @@ function generateToken(username, ip){
 	while(1){
 		token =  Math.floor(Math.random() * (1000000000000 - 100000000) + 100000000) + username + ip;
 		let checks = 0;
-		for (i = 0; i<current_users.length; i++){
-			if (token == current_users[i].token){
+		for (i = 0; i<sessionList.length; i++){
+			if (username == sessionList[i].user){
+				throw "User already signed in!";
+			}
+			if (token == sessionList[i].token){
 				break;
 			}
 			checks++;
 		}
-		if (checks == current_users.length){
+		if (checks == sessionList.length){
 			return token;
 		}
 	}
@@ -22,39 +35,49 @@ function getToken(username, password, ip){
 	//Add password encryption
 	//Checks username and password match
 	if (username == "jess" && password == "password"){
-		return generateToken(username, ip);
+		try{
+			return generateToken(username, ip);
+		}catch(error){
+			console.log(error);
+			throw error;
+		}
+
 	}
 	else{
 		//Failed to getToken
 		//Password and Username do not match
-		return -1;
+		throw "Failed to get token: User/Password mismatch";
 	}
 }
 
-function authenticate(user, ip){
-	const token = getToken("jess", "password", ip);
-	current_users.push({"username":user, "ip":ip, "token":token});
-	console.log(current_users);
-	return token;
+function authenticate(user, password, ip){
+	try{
+		const token = getToken(user, password, ip);
+		sessionList.push(new session(user, ip, token));
+		console.log(sessionList);
+		return token;
+	}catch (error){
+		throw error
+	}
+
 }
 
-//returns 1 on success
-//returns -1 on auth_obj parsing error
-//returns -2 on non authenticated token
+//Check the value of a token object against current session tokens
 function validate_token(auth_obj){
 	try {
 		auth_obj = JSON.parse(auth_obj);
 	}
-	catch(e) {
-		console.log(e);
+	catch(error) {
+		console.log(error);
 		throw "Failed to Authorize: Invalid Auth Token Format!";
 	}
-	for (i = 0; i<current_users.length; i++){
-		console.log("Token: " + current_users[i].token);
-		console.log("Client Token: " + auth_obj.token + ", typeof auth_obj:" + typeof auth_obj);
-		if (auth_obj.token == current_users[i].token){
-			console.log("Found a Token Match:" + auth_obj.token + ", username:" + current_users[i].username);
-			if (auth_obj.username == current_users[i].username){
+
+	for (i = 0; i<sessionList.length; i++){
+		//console.log("Token: " + sessionList[i].token);
+		//console.log("Client Token: " + auth_obj.token + ", typeof auth_obj:" + typeof auth_obj);
+		if (auth_obj.token == sessionList[i].token){
+			//console.log("Found a Token Match:" + auth_obj.token + ", username:" + sessionList[i].user);
+			if (auth_obj.user == sessionList[i].user){
 				return 1;
 			}
 		}
@@ -63,7 +86,7 @@ function validate_token(auth_obj){
 }
 
 function getCurrentUsers(){
-	return this.current_users;
+	return sessionList;
 }
 
 exports.authenticate = authenticate;
