2a8c9a09367a2788f250c20e00b636dac1621e38
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-01-26 11:06:21 -0800

    Converted from spaces to tabs

--- cob-fianance-app/src/App.js
+++ cob-fianance-app/src/App.js
@@ -16,29 +16,29 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component{
-  state = {
-    todos: []
-  }
-  render(){
-  return (
-    <Router>
-    <div className="App" style={{background: '#d3d3d3', backgroundSize: 'cover',   width: '100%' , height: '100vh' , minHeight: '100%' }}>
-      <Navibar />
-      <div className="container">
-      <Route exact path="/" component={Login} />
-      <Route path="/home" component={Login} />
-      <Route exact path="/DashboardI" component={DashboardI} />
-      <Route exact path="/UserManagement" component={UserManagement} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/transactions" component={Transactions} />
-      <Route path="/expenses" component={Expenses} />
-      <Route path="/profile" component={Login} />
-      <Route path="/querytestpage" component={QueryTestPage} />
-      </div>
-      </div>
-      </Router>
-      );
-    }
-  }
+	state = {
+		todos: []
+	}
+	render(){
+		return (
+			<Router>
+				<div className="App" style={{background: '#d3d3d3', backgroundSize: 'cover',   width: '100%' , height: '100vh' , minHeight: '100%' }}>
+					<Navibar />
+					<div className="container">
+						<Route exact path="/" component={Login} />
+						<Route path="/home" component={Login} />
+						<Route exact path="/DashboardI" component={DashboardI} />
+						<Route exact path="/UserManagement" component={UserManagement} />
+						<Route path="/dashboard" component={Dashboard} />
+						<Route path="/transactions" component={Transactions} />
+						<Route path="/expenses" component={Expenses} />
+						<Route path="/profile" component={Login} />
+						<Route path="/querytestpage" component={QueryTestPage} />
+					</div>
+				</div>
+			</Router>
+		);
+	}
+}
 
 export default App;
