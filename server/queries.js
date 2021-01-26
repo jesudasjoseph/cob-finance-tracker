8f66bbff64a27e599326d29e052cde56e6085083
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-01-26 11:45:25 -0800

    Added basic database connection and query for testing on startup

---
+++ server/queries.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+const pool = new Pool();
+
+function init(){
+	pool.on('error', (err, client) => {
+		console.error('Unexpected error on idle client', err)
+		process.exit(-1)
+	})
+}
+
+function getBid(uid){
+	// callback - checkout a client
+	pool.connect((err, client, done) => {
+		if (err) throw err
+		client.query(`SELECT bid FROM user WHERE uid = ` + uid, [1], (err, res) => {
+			done()
+			if (err) {
+				console.log(err.stack)
+			} else {
+				console.log(res.rows[0])
+			}
+		})
+	})
+}
+
+exports.init = init;
+exports.getBid = getBid;
