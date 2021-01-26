300026100eb612fc8306e7169d3167017781fb6c
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-01-25 18:46:53 -0800

    Integrated Login Page to collect token associated with UID provided

--- cob-fianance-app/src/components/pages/Login.js
+++ cob-fianance-app/src/components/pages/Login.js
@@ -1,21 +1,64 @@
-import React from 'react'
+import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-export default function Login() {
-    return (
-        <React.Fragment >
-            <h2 style={{paddingTop:'25px' , textAlign:'center'}} >Sign in with your ONID credentials</h2>
-            <div style={{flex: '1', paddingTop:'100px', paddingBottom:'50px' , textAlign: 'center' }}>
-            <>
-            <Button style={{flex: '1' , textAlign: 'center', width:'700px',   display: 'inline-block', height: '100px' }} as="input" type="button" value="Login as Student" />{' '}
-            </>
-            </div>
-            <div style={{ flex: '1', padding: '50px 100px' , textAlign: 'center'}}>
-             <>
-             <Button style={{flex: '1' , textAlign: 'center', width:'700px',   display: 'inline-block', height: '100px' }} as="input" type="button" value="Login as Instructor" />{' '}
-             </>
-             </div>
+export default class Login extends React.Component {
 
-        </React.Fragment>
-    )
+    constructor(props) {
+        super(props);
+        this.state = {
+            uid: "",
+            token: ""
+        };
+
+        this.getUserToken = this.getUserToken.bind(this);
+        this.handleUidChange = this.handleUidChange.bind(this);
+    }
+
+    handleUidChange(e) {
+        this.setState({uid: e.target.value});
+    }
+
+    getUserToken() {
+        let body = {uid:this.state.uid};
+
+		fetch('http://71.193.191.23:2021/auth', {
+			mode: 'cors',
+			method: 'POST',
+			credentials: 'same-origin',
+			headers: {
+				'Accept': 'application/json',
+				'Content-type': 'application/json'
+			},
+			body: JSON.stringify(body)
+        }).then(response => {
+			return response.json();
+		}).then(data => {
+			if (data.token){
+				this.setState({token: data.token});
+			} else {
+				this.setState({token: 0});
+			}
+			console.log('Success:', data);
+		}).catch((error) => {
+			console.error('Error:', error);
+		});
+    }
+
+    render () {
+        return (
+            <React.Fragment >
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>User ID</Form.Label>
+                        <Form.Control type="input" placeholder="Enter UID" onChange={this.handleUidChange} />
+                    </Form.Group>
+                    <Button onClick={this.getUserToken} variant="primary">
+                        Student
+                    </Button>
+                </Form>
+            </React.Fragment>
+        );
+    }
+
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 let authorizor = require('../authorizor');
 
 /*GET request for /auth (returns token)*/
+/*
 router.get('/', function(req, res, next) {
 	let token;
 	res.statusCode = 200;
@@ -17,13 +18,14 @@ router.get('/', function(req, res, next) {
 	res.send(JSON.stringify({"token":token}));
 
 });
+*/
 
 //Get Credentials and Authenticate! (user = jess and password = password as default)
 router.post('/', function(req, res, next) {
 	let token;
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'application/json');
-	token = authorizor.getToken(req.body.user, req.ip);
+	token = authorizor.getToken(req.body.uid, req.ip);
 	res.send(JSON.stringify({"token":token}));
 
 });
