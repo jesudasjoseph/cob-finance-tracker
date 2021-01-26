e602836186cc31c6947aa69bc694a3c010cfa819
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-01-25 20:44:22 -0800

    Login Page now redirects if a token already exists. Store tokens in local storage

--- cob-fianance-app/src/components/pages/Login.js
+++ cob-fianance-app/src/components/pages/Login.js
@@ -5,14 +5,28 @@ import Form from 'react-bootstrap/Form';
 export default class Login extends React.Component {
 
     constructor(props) {
+        let myStorage = window.localStorage;
         super(props);
         this.state = {
             uid: "",
-            token: ""
+            token: null
         };
 
         this.getUserToken = this.getUserToken.bind(this);
         this.handleUidChange = this.handleUidChange.bind(this);
+
+        if (myStorage.getItem('token') != undefined ) {
+            if (myStorage.getItem('role') === 'student' ) {
+                props.history.push('/dashboard');
+            }
+            else if (myStorage.getItem('role') === 'instructor') {
+                props.history.push('/DashboardI');
+            }
+            else {
+                myStorage.removeItem('token');
+                myStorage.removeItem('role');
+            }
+        }
     }
 
     handleUidChange(e) {
@@ -20,7 +34,11 @@ export default class Login extends React.Component {
     }
 
     getUserToken() {
+        let myStorage = window.localStorage;
         if (this.state.token != null){
+            alert("User already has a Token!");
+        }
+        else {
             let body = {uid:this.state.uid};
 
     		fetch('http://71.193.191.23:2021/auth', {
@@ -37,18 +55,32 @@ export default class Login extends React.Component {
     		}).then(data => {
     			if (data.token){
     				this.setState({token: data.token});
+                    if (data.role === "instructor"){
+                        this.setState({role: data.role});
+                        myStorage.setItem('jwt',"Bearer " + this.state.token);
+                        myStorage.setItem('role', data.role);
+                        this.props.history.push('/DashboardI');
+                    }
+                    else if (data.role === "student"){
+                        this.setState({role: data.role});
+                        myStorage.setItem('jwt',"Bearer " + this.state.token);
+                        myStorage.setItem('role', data.role);
+                        this.props.history.push('/dashboard');
+                    }
+                    else {
+                        alert("No role specified!");
+                        this.setState({token: 0});
+                    }
+                    //redirect to instructor or student account
     			} else {
     				this.setState({token: 0});
+                    alert("Failed to get Token!");
     			}
     			console.log('Success:', data);
     		}).catch((error) => {
     			console.error('Error:', error);
     		});
         }
-        else {
-            alert("User already has a Token!");
-        }
-
     }
 
     render () {
