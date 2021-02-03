89219192251913e5492d2101bfd6c10b8eeb6dd6
Ghaith Shan <49415608+hoith@users.noreply.github.com>
2021-02-02 16:01:43 -0800

    transaction table

--- cob-fianance-app/src/components/Layout/TransactionsTable.js
+++ cob-fianance-app/src/components/Layout/TransactionsTable.js
@@ -2,16 +2,35 @@ import React, { Component } from 'react'
 import Table from 'react-bootstrap/Table';
 
 
-function createData(id,date, name, Customer, Location, Paymethod, product , quan , price, total) {
+function createData(date, name, Customer, Location, Paymethod, product , quan , price, total) {
     return { date, name, Customer, Location, Paymethod, product , quan , price, total};
   }
   const rows =[
     createData('1', '01', 'me', 'customer1', 'location1', 'Paymethod1','goods1', 54,100 , 12341234),
     createData('2','02', 'me2', 'customer2', 'location2', 'Paymethod2', 'goods2' , 59,100 , 543543)
   ]
+  function addTransaction(rows){
+    rows.push(createData('3', '01', 'me', 'customer1', 'location1', 'Paymethod1','goods3', 54,100 , 12341234))
+  }
+
 export class Tables extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
     render() {
         return (
+          <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Name:
+              <input type="text" name="name" />
+              </label>
+              <input type="submit" value="Submit" />
+              </form>
             <Table responsive="sm" size="xl" style={{paddingBottom:'40px' , paddingTop: '10px'}} striped bordered hover variant="dark">
                 <thead>
                     <tr>
@@ -42,6 +61,7 @@ export class Tables extends Component {
           ))}
         </tbody>
       </Table>
+      </div>
   );
     }
 }
