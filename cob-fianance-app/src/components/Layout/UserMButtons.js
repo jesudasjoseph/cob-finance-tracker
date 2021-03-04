2408a0772a94e3f56b3e8a3192f997b152072552
jack sir <>
2021-03-04 14:53:08 -0800

    These are the changes I've made for the table aloc

--- cob-fianance-app/src/components/Layout/UserMButtons.js
+++ cob-fianance-app/src/components/Layout/UserMButtons.js
@@ -4,11 +4,6 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Nav from 'react-bootstrap/Nav';
 
 export class UserMButtons extends Component {
-  handleclick(event){
-    alert('You clicked Instructor ');
-    event.preventDefault();
-  }
-
     render() {
         return (
           <>
@@ -16,8 +11,7 @@ export class UserMButtons extends Component {
         <Button as="input" type="button" value="Import Students" />{' '}
         <Nav>
         <NavDropdown title="Filter By" id="collasible-nav-dropdown">
-          <NavDropdown.Item href="#filterI" OnClick = {this.handleclick} >Instructor</NavDropdown.Item>
-          <NavDropdown.Item href="#filterC">Class</NavDropdown.Item>
+          <NavDropdown.Item href="#filterI">Instructor</NavDropdown.Item>
           <NavDropdown.Item href="#filterS">Section</NavDropdown.Item>
         </NavDropdown>
         </Nav>
