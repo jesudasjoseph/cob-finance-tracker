c7c44e72767092bef787c406e8caff802120a3ed
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-04-14 08:59:38 -0700

    changed API root path /api/1

--- server/server.js
+++ server/server.js
@@ -21,32 +21,38 @@ let expenseRouter = require('./routes/expense');
 let depositRouter = require('./routes/deposit');
 let exportRouter = require('./routes/export');
 
+const version = 1;
+
+const apiPath = '/api/' + version;
+
 //app.use(helmet()); //Use helmet as a middleware to help with http header security
 app.use(cors()); //Use cors middleware
 app.use(express.json()); //Parse body
 app.use(express.static(path.join(__dirname, 'build'),)); //Use Static Website Build Path
 
+//Ping
 app.get('/ping', (req, res) => {
   return res.send('pong')
 })
 
-console.log(path.resolve(__dirname, 'build', 'index.html'));
-
+//API Endpoints
 //Router for Authentication requests
-app.use('/auth', authRouter);
+app.use(apiPath + '/auth', authRouter);
 //Router for Business data requests
-app.use('/business', businessRouter);
+app.use(apiPath + '/business', businessRouter);
 //Router for User data requests
-app.use('/user', userRouter);
+app.use(apiPath + '/user', userRouter);
 //Router for Transaction data requests
-app.use('/transaction', transactionRouter);
+app.use(apiPath + '/transaction', transactionRouter);
 //Router for Expense data requests
-app.use('/expense', expenseRouter);
+app.use(apiPath + '/expense', expenseRouter);
 //Router for Deposit data requests
-app.use('/deposit', depositRouter);
+app.use(apiPath + '/deposit', depositRouter);
 //Router for Export data requests
-app.use('/export', exportRouter);
+app.use(apiPath + '/export', exportRouter);
 
+
+//Serve Static Site
 app.get(['/', '/*'], (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
