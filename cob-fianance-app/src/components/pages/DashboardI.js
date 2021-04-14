d3ed88a7160a98a44bbd899d6f9e7d82ebbea632
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-04-13 18:03:42 -0700

    cleaned up Instructor Dashboard

--- cob-fianance-app/src/components/pages/DashboardI.js
+++ cob-fianance-app/src/components/pages/DashboardI.js
@@ -3,14 +3,11 @@ import GroupTable from '../Layout/GroupTable';
 import NavibarI from '../Layout/MyNavBarI';
 
 export default function DashboardI() {
-    return (
-      <React.Fragment>
-      <NavibarI/>
-          <h1 style={{textAlign:'center'}}> Dashboard </h1>
-          <div style={{textAlign: 'right'}}>
-          </div>
-          <h2 style={{textAlign:'left'}}> Welcome Instructor </h2>
-          <GroupTable style = {{paddingTop: '10px 20px'}}></GroupTable>
-      </React.Fragment>
-  )
+	return (
+		<React.Fragment>
+			<NavibarI/>
+			<h1 style={{textAlign:'center'}}> Dashboard </h1>
+			<GroupTable/>
+		</React.Fragment>
+	)
 }
