d1a34cd61663eaf68cc89ca8ca58dec2cf4eef05
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-04-19 11:02:18 -0700

    added auth

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const config = require('./config');
 const path = require('path');
 const passport = require('passport');
 let SamlStrategy = require('passport-saml').Strategy;
+const authorizor = require('./authorizor');
 const q = require('./queries');
 
 let key = fs.readFileSync(__dirname + '/certs/selfsigned.key');
@@ -48,8 +49,9 @@ passport.use(new SamlStrategy({
 		callbackUrl: 'https://71.193.191.23:2020/saml/consume',
 		cert: 'MIIEKjCCApKgAwIBAgIVAI6dNgrLT0XnII3Wm2hwwErVz99lMA0GCSqGSIb3DQEB CwUAMCgxJjAkBgNVBAMMHWxvZ2luLWludC5pYW0ub3JlZ29uc3RhdGUuZWR1MB4X DTIwMTIwMzA3MDYwM1oXDTQwMTIwMzA3MDYwM1owKDEmMCQGA1UEAwwdbG9naW4t aW50LmlhbS5vcmVnb25zdGF0ZS5lZHUwggGiMA0GCSqGSIb3DQEBAQUAA4IBjwAw ggGKAoIBgQCWZj4Q7rgoW8XxaAlU8pIqCC1SNTyk819sw/iIGlByL3xA8qCHjxQ/ TNVQgJ4N1EHyUjjwMmIHJMR+xWs1AROPIkuQtYGA+eZpU9urow6kpSnP2fBc4rY2 LhD3hTI3PBnDdFooLKWqw8rXTDpcPV/6KvKsyeOt8AlvnNmtVzwUqqbpRROs+VPo YcAbqj2z/Igwr1yrhTvNFsA7mx0zUZyrS16SR0LeOeYkcs7zx2xJe8wLhfxtQfUU sd20tKhJCB5zfexJDVweBn27K12rDqHkaQt0Dzn52h2mVeiy4DcNr4u5XMh2M+W3 WTpveRWNJVY1xr5ypB3A9yD4GEBkzQocNXuBvnwhGnOeBZTksMkHPkuj7NG2aCjK LOW6Ai+WbKbUHL0nEqWo+vwUNN/9yM5fPxHcapRUtFcLeOnLasuaGh7e4IYN6emb FjHSd8qFHdjofU8vH+U/vsaU+AtETGC3r9I/TeSvphxHRMJJz2/WrqPHQ/u+VlxR 8nx/eiwcndMCAwEAAaNLMEkwHQYDVR0OBBYEFECGNvNQAMiH7F++jlyEVkM2rEoi MCgGA1UdEQQhMB+CHWxvZ2luLWludC5pYW0ub3JlZ29uc3RhdGUuZWR1MA0GCSqG SIb3DQEBCwUAA4IBgQBO9/zaYkcRBWMcoJQxm2CIibey9QV6KcuRU7Xgnew+uHhG d/pEjfpq6jpLkbVuT7tToduYhPKDDK8bylF5y2kKuSKs2QWW0ST0qIwbJy4cGiZq fTGhl8j9PlUHeLlbID0Bvhm1rS8YfZWwcIGTol8FtlDidIy9ZdA6+Y86uylpEK3J YQyoPTl0LCgR2J5dM6KBr8F5dUlUM8EaDfcjopkpC3WW5+kpLszlKZrgcnVo31eu ckLTD9a6EbJpkjP+TpeA6er2quPgITnMQ+3Uo3sxKKWPDsHQuOXr60nxx3d7lRcS k6242b2rZZYxawmrA1POFEHxuZCbIJMEJR/VSG8djt98YLFbh8fg6uiF+Ybl/VGy xx6aTYaW+cKt2ITqUzhsCHdFXa6aWOjCMAY5bKXP15r+xl9F/akg3Qxg2oyGIFMC +h+wbBymHxtV5ThBTrkViNPNtId5K8l/p0g3df0pQZTIfp//9ZJryOKQ3x5MSKml nGIyTZMKkASmL8a+C9M='
 	},
-	function(profile, done) {
-		console.log('profile', profile);
+	async (profile, done) => {
+		let {res} = await authorizor.getToken(profile.nameID.split('@')[0], 0);
+		return done(null, res)
 	}));
 
 app.get('/l',
@@ -65,6 +67,7 @@ app.post('/saml/consume',
 	passport.authenticate('saml', { failureRedirect: '/', failureFlash: true }),
 	(req, res) => {
 		console.log("SAML Consumed!");
+		console.log(req);
 		res.redirect('/');
 	});
 
