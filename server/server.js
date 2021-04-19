b30e52e847333bc5df83d9edb18650112968e403
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-04-19 10:28:27 -0700

    added a simple sso strategy.

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const bodyparser = require('body-parser');
 const https = require('https');
 const config = require('./config');
 const path = require('path');
+let SamlStrategy = require('passport-saml').Strategy;
 const q = require('./queries');
 
 let key = fs.readFileSync(__dirname + '/certs/selfsigned.key');
@@ -37,10 +38,27 @@ app.use(cors()); //Use cors middleware
 app.use(express.json()); //Parse body
 app.use(express.static(path.join(__dirname, 'build'),)); //Use Static Website Build Path
 
+passport.use(new SamlStrategy({
+		entryPoint: 'https://login-int.iam.oregonstate.edu/idp/profile/SAML2/Redirect/SSO',
+		issuer: 'https://71.193.191.23:2020',
+		callbackUrl: 'https://71.193.191.23:2020/saml/consume',
+		cert: 'MIIEKjCCApKgAwIBAgIVAI6dNgrLT0XnII3Wm2hwwErVz99lMA0GCSqGSIb3DQEBCwUAMCgxJjAkBgNVBAMMHWxvZ2luLWludC5pYW0ub3JlZ29uc3RhdGUuZWR1MB4XDTIwMTIwMzA3MDYwM1oXDTQwMTIwMzA3MDYwM1owKDEmMCQGA1UEAwwdbG9naW4taW50LmlhbS5vcmVnb25zdGF0ZS5lZHUwggGiMA0GCSqGSIb3DQEBAQUAA4IBjwAwggGKAoIBgQCWZj4Q7rgoW8XxaAlU8pIqCC1SNTyk819sw/iIGlByL3xA8qCHjxQ/TNVQgJ4N1EHyUjjwMmIHJMR+xWs1AROPIkuQtYGA+eZpU9urow6kpSnP2fBc4rY2LhD3hTI3PBnDdFooLKWqw8rXTDpcPV/6KvKsyeOt8AlvnNmtVzwUqqbpRROs+VPoYcAbqj2z/Igwr1yrhTvNFsA7mx0zUZyrS16SR0LeOeYkcs7zx2xJe8wLhfxtQfUUsd20tKhJCB5zfexJDVweBn27K12rDqHkaQt0Dzn52h2mVeiy4DcNr4u5XMh2M+W3WTpveRWNJVY1xr5ypB3A9yD4GEBkzQocNXuBvnwhGnOeBZTksMkHPkuj7NG2aCjKLOW6Ai+WbKbUHL0nEqWo+vwUNN/9yM5fPxHcapRUtFcLeOnLasuaGh7e4IYN6embFjHSd8qFHdjofU8vH+U/vsaU+AtETGC3r9I/TeSvphxHRMJJz2/WrqPHQ/u+VlxR8nx/eiwcndMCAwEAAaNLMEkwHQYDVR0OBBYEFECGNvNQAMiH7F++jlyEVkM2rEoiMCgGA1UdEQQhMB+CHWxvZ2luLWludC5pYW0ub3JlZ29uc3RhdGUuZWR1MA0GCSqGSIb3DQEBCwUAA4IBgQBO9/zaYkcRBWMcoJQxm2CIibey9QV6KcuRU7Xgnew+uHhGd/pEjfpq6jpLkbVuT7tToduYhPKDDK8bylF5y2kKuSKs2QWW0ST0qIwbJy4cGiZqfTGhl8j9PlUHeLlbID0Bvhm1rS8YfZWwcIGTol8FtlDidIy9ZdA6+Y86uylpEK3JYQyoPTl0LCgR2J5dM6KBr8F5dUlUM8EaDfcjopkpC3WW5+kpLszlKZrgcnVo31euckLTD9a6EbJpkjP+TpeA6er2quPgITnMQ+3Uo3sxKKWPDsHQuOXr60nxx3d7lRcSk6242b2rZZYxawmrA1POFEHxuZCbIJMEJR/VSG8djt98YLFbh8fg6uiF+Ybl/VGyxx6aTYaW+cKt2ITqUzhsCHdFXa6aWOjCMAY5bKXP15r+xl9F/akg3Qxg2oyGIFMC+h+wbBymHxtV5ThBTrkViNPNtId5K8l/p0g3df0pQZTIfp//9ZJryOKQ3x5MSKmlnGIyTZMKkASmL8a+C9M='
+	},
+	function(profile, done) {
+
+	}));
+
 //ping
 app.get('/ping', (req, res) => {
   return res.send('pong')
 })
+
+app.post('/saml/consume',
+	bodyparser.urlencoded({extended: false}),
+	passport.authenticate('saml', {failureRedirect: '/', failureFlash: true}),
+	(req, res) => {
+		res.redirect('/');
+	});
 
 console.log(path.resolve(__dirname, 'build', 'index.html'));
 
