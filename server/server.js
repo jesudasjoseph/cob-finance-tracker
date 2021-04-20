16b14f255d4de8fc693765467c7ae303979c1819
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-04-20 16:20:55 -0700

    tried another routing change

--- server/server.js
+++ server/server.js
@@ -61,7 +61,7 @@ app.use(API_URL + '/deposit', depositRouter);
 app.use(API_URL + '/export', exportRouter);
 
 //Static Server (Front-End)
-app.get(['/', '/*'], (req, res) => {
+app.get(['/', '*'], (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
