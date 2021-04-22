ff6f0c94e2a6458ef15a7f931366b4281efadcc9
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-04-22 09:02:46 -0700

    Changed ExpenseProgress to use props instead of fetching its own data.

--- cob-fianance-app/src/components/Layout/ExpenseProgress.js
+++ cob-fianance-app/src/components/Layout/ExpenseProgress.js
@@ -2,68 +2,18 @@ import React, { Component } from 'react'
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import { API_PATH } from '../Config';
 
-const now = 30;
-var resProfit = ''
-var resExpense = ''
-var a = 0
-var b = 0
-var c = 0
-var d =0
-var goal = 100
-var resRevenue=''
 export class ExpenseProgress extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            profitbar: '',
-            expensesbar: '',
-            revenuebar:'',
-            businessTable: []
-          , expenses:[]}
-            this.get_business = this.get_business.bind(this);
-            this.get_business();
-            this.state.betternow = this.deposit_total;
-              }
-      get_business(){
-        fetch(API_PATH + '/business/byuid', {
-          mode: 'cors',
-          method: 'GET',
-          credentials: 'same-origin',
-          headers: {
-            'Accept': 'application/json',
-            'Content-type': 'application/json',
-            'Authorization': window.localStorage.getItem('jwt')
-          }
-        }).then(response => {
-          console.log(response);
-          return response.json();
-        }).then(data => {
-          console.log('Success:', data);
-          this.setState({expenses:data});
-        }).catch((error) => {
-          console.error('Error:', error);
-        });
-      }
-    render() {
-        {this.state.expenses.map((expense, index) => {
-            const {deposit_count,deposit_total,expense_count,expense_total, name, product_count, profit,transaction_total,transaction_count} = expense;
-            this.state.profitbar = profit
-            this.state.expensesbar = expense_total
-            this.state.revenuebar = transaction_total
-            a = this.state.revenuebar
-            b = this.state.expensesbar
-
-            return (
-              <div>{expense_total}</div>
-            )
-            })}
-        return(
-        <div>
-            <ProgressBar  style= {{height:'50px'}} variant="danger" now={100*b/goal} label={`Expenses: $${b}`} />
-            <ProgressBar style= {{height:'50px'}} variant="success" now={100*a/goal} label={`Revenue: $${a}`}/>
-        </div>
-        )
-    }
+	constructor(props){
+		super(props);
+	}
+	render() {
+		return(
+			<>
+				<ProgressBar  style= {{height:'50px'}} variant="danger" now={100*this.props.expenses/(this.props.expenses > this.props.revenue ? this.props.expenses : this.props.revenue)} label={`Expenses: $${this.props.expenses}`} />
+				<ProgressBar style= {{height:'50px'}} variant="success" now={100*this.props.revenue/(this.props.expenses > this.props.revenue ? this.props.expenses : this.props.revenue)} label={`Revenue: $${this.props.revenue}`}/>
+			</>
+		)
+	}
 }
 
 export default ExpenseProgress
