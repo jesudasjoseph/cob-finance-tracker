b8a8af733e56b60a3988108340e020aec45c0d37
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-04-29 12:29:06 -0700

    Rerouted student pages

--- cob-fianance-app/src/App.js
+++ cob-fianance-app/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router ,Switch, Route} from 'react-router-dom'
 import Dashboard from './components/pages/Dashboard';
 import DashboardI from './components/pages/DashboardI';
 import InstructorNavbar from './components/Layout/InstructorNavbar';
+import StudentNavbar from './components/Layout/StudentNavbar';
 import UserInstructor from './components/pages/UserInstructor';
 import SettingsInstructor from './components/pages/SettingsInstuctor';
 import UserManagement from './components/pages/UserManagement';
@@ -34,17 +35,24 @@ class App extends Component{
 							<Switch>
 								<Route exact path="/instructor/dashboard" component={DashboardI}/>
 								<Route exact path="/instructor/settings" component={SettingsInstructor} />
+								<Route exact path="/instructor/profile" component={UserInstructor} />
 								<Route exact path="/instructor/usermanagement" component={UserManagement} />
 								<Route exact path="/instructor/dashboard/:id" component={SnapshotGroup}/>
 								<Route path="*" component={Page404}/>
 							</Switch>
 						</Route>
 
-						<Route path= "/Profile" component={UserInstructor} />
-						<Route path="/dashboard" component={Dashboard} />
-						<Route path="/transactions" component={Transactions} />
-						<Route path="/expenses" component={Expenses} />
-						<Route path="/settings" component={Page_Settings}/>
+						<Route path="/student">
+							<StudentNavbar/>
+							<Switch>
+								<Route exact path="/student/dashboard" component={Dashboard}/>
+								<Route exact path="/student/settings" component={Page_Settings} />
+								<Route exact path="/student/transactions" component={Transactions} />
+								<Route exact path="/student/expenses" component={Expenses}/>
+								<Route path="*" component={Page404}/>
+							</Switch>
+						</Route>
+
 						<Route path="*" component={Page404}/>
 						</Switch>
 				</div>
