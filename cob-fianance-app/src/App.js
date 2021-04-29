00f79dd7cb581e6a55d7f818158f71a9fe0f37a4
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-04-29 12:23:39 -0700

    changed boolean prop

--- cob-fianance-app/src/App.js
+++ cob-fianance-app/src/App.js
@@ -23,7 +23,7 @@ class App extends Component{
 	}
 	render(){
 		return (
-			<Router forceRefresh='False'>
+			<Router forceRefresh={false}>
 				<div>
 					<Switch>
 						<Route exact path="/" component={Login} />
