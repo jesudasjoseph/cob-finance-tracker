05a67bb0408fbdff12ca32bf16c1bc73e5488029
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-05-04 12:51:55 -0700

    Formatted tables

--- cob-fianance-app/src/components/pages/instructor/DepositDashboard.js
+++ cob-fianance-app/src/components/pages/instructor/DepositDashboard.js
@@ -1,25 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Table from 'react-bootstrap/Table';
 import { API_PATH } from '../../Config';
 
 export default class DepositDashboard extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			bid: this.props.location.pathname.split('/')[this.props.location.pathname.split('/').length-1],
+			selectedBid: null,
 			depositData: [],
-			businessData: []
+			businessData: [],
+			businessName: ''
 		};
 
 		this.fetchDepositData = this.fetchDepositData.bind(this);
 		this.fetchBusinessData = this.fetchBusinessData.bind(this);
+
+		this.businessTableRowClickHandler = this.businessTableRowClickHandler.bind(this);
 	}
 
 	componentDidMount(){
-		this.fetchDepositData();
+		this.fetchBusinessData();
 	}
 
 	fetchBusinessData(){
-		fetch(API_PATH + '/business/bybid?bid=' + this.state.bid, {
+		fetch(API_PATH + '/business?start=0&end=50', {
 			mode: 'cors',
 			method: 'GET',
 			credentials: 'same-origin',
@@ -38,9 +42,8 @@ export default class DepositDashboard extends React.Component{
 			console.error('Error:', error);
 		});
 	}
-
-	fetchDepositData(){
-		fetch(API_PATH + '/deposit?start=0&end=50&bid=' + this.state.bid, {
+	fetchDepositData(bid, name){
+		fetch(API_PATH + '/deposit?start=0&end=50&bid=' + bid, {
 			mode: 'cors',
 			method: 'GET',
 			credentials: 'same-origin',
@@ -54,22 +57,106 @@ export default class DepositDashboard extends React.Component{
 			return response.json();
 		}).then(data => {
 			console.log('Success:', data);
-			this.setState({depositData:data});
+			this.setState({selectedBid:bid, businessName:name, depositData:data});
 		}).catch((error) => {
 			console.error('Error:', error);
 		});
 	}
 
-	render () {
-		return (
-			<React.Fragment>
-				<div>
-
-				</div>
-				<div>
-				</div>
-			</React.Fragment>
-		)
+	businessTableRowClickHandler(bid, name){
+		this.fetchDepositData(bid, name);
 	}
 
+	render () {
+		if (this.state.depositData != null && this.state.depositData.length ) {
+			return (
+				<React.Fragment>
+					<div  style={{display: 'grid', gridTemplateColumns: '20% 70% 10%'}}>
+						<div style={{margin: '20px'}}>
+							<Table responsive="sm" size="xl" striped bordered hover variant="dark">
+								<thead>
+									<tr>
+										<th>Business</th>
+									</tr>
+								</thead>
+								<tbody>
+									{this.state.businessData.map((business, index) => {
+										const {name, bid} = business;
+										return (
+											<tr key={bid} style={{cursor: 'pointer'}} onClick={() => this.businessTableRowClickHandler(bid, name)}>
+												<td>{name}</td>
+											</tr>
+										);
+									})}
+								</tbody>
+							</Table>
+						</div>
+						<div style={{margin: '20px'}}>
+							<h2>{this.state.businessName}</h2>
+							<Table responsive="sm" size="xl" striped bordered hover variant="dark">
+								<thead>
+									<tr>
+										<th>Date</th>
+										<th>Amount</th>
+										<th>ONID</th>
+										<th>Description</th>
+									</tr>
+								</thead>
+								<tbody>
+									{this.state.depositData.map((deposit, index) => {
+										const {d_id, date, uid, val, description} = deposit;
+										return (
+											<tr key={d_id}>
+												<td>{date.split('T')[0]}</td>
+												<td>{val}</td>
+												<td>{uid}</td>
+												<td>{description}</td>
+											</tr>
+										);
+									})}
+								</tbody>
+							</Table>
+						</div>
+						<div>
+
+						</div>
+					</div>
+				</React.Fragment>
+			);
+		}
+		else {
+			return (
+				<React.Fragment>
+				<div style={{display: 'grid', gridTemplateColumns: '20% 70% 10%'}}>
+					<div style={{margin: '20px'}}>
+						<Table responsive="sm" size="xl" striped bordered hover variant="dark">
+							<thead>
+								<tr>
+									<th>Business</th>
+								</tr>
+							</thead>
+							<tbody>
+								{this.state.businessData.map((business, index) => {
+									const {name, bid} = business;
+									return (
+										<tr key={bid} style={{cursor: 'pointer'}} onClick={() => this.businessTableRowClickHandler(bid, name)}>
+											<td>{name}</td>
+										</tr>
+									);
+								})}
+							</tbody>
+						</Table>
+					</div>
+					<div style={{margin: '20px'}}>
+						<h2>{this.state.businessName}</h2>
+						<h4 style={{color: 'red'}}>This business has no deposits!</h4>
+					</div>
+					<div>
+
+					</div>
+				</div>
+				</React.Fragment>
+			);
+		}
+	}
 }
