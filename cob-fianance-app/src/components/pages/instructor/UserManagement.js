c902b9ca8fa4138503d9dbb5b98457d9689a22c2
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-05-08 16:13:05 -0700

    removed un-needed tag. and copied AddStudentDialogButton.js

--- cob-fianance-app/src/components/pages/instructor/UserManagement.js
+++ cob-fianance-app/src/components/pages/instructor/UserManagement.js
@@ -5,7 +5,7 @@ export default function StudentUsers() {
 	return (
 		<React.Fragment>
 			<h1 style={{textAlign:'center'}}>User Management</h1>
-			<Table style = {{paddingTop: '10px 20px'}}></Table>
+			<Table style = {{paddingTop: '10px 20px'}}/>
 		</React.Fragment>
 	)
 }
