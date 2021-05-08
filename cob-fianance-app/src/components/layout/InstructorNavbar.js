5472d260e2febf81b90f1e33c6610fa74941ed1f
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-05-08 13:35:55 -0700

    Removed un-used import line.

--- cob-fianance-app/src/components/layout/InstructorNavbar.js
+++ cob-fianance-app/src/components/layout/InstructorNavbar.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import {Link} from 'react-router-dom';
 
 class InstructorNavbar extends Component {
