2c7a8f68b6c9b797d226bc5ff195feaf0565460e
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-05-14 08:52:08 -0700

    fixed navbars.

--- cob-fianance-app/src/components/layout/InstructorNavbar.js
+++ cob-fianance-app/src/components/layout/InstructorNavbar.js
@@ -7,13 +7,13 @@ class InstructorNavbar extends Component {
 	render(){
 		return(
 			<Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-				<Navbar.Brand as={Link} to="/home">College of Buisness App</Navbar.Brand>
+				<Navbar.Brand as={Link} to="/instructor/dashboard">College of Buisness App</Navbar.Brand>
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 				<Navbar.Collapse id="responsive-navbar-nav">
 					<Nav className="mr-auto">
 						<Nav.Link as={Link} to="/instructor/dashboard">Dashboard</Nav.Link>
 						<Nav.Link as={Link} to="/instructor/bank">Bank</Nav.Link>
-						<Nav.Link as={Link} to="/instructor/usermanagement">UserManagement</Nav.Link>
+						<Nav.Link as={Link} to="/instructor/usermanagement">Users</Nav.Link>
 					</Nav>
 					<Nav>
 						<Nav.Link as={Link} to="/home" onClick={() => {window.localStorage.clear();}}>Logout</Nav.Link>
