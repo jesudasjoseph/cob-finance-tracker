90f6ff10413074b8af3b0332e72b516436fe7b36
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-07-07 18:35:03 -0700

    Fixed clipped text issue. Broke ProfitProgress

--- cob-fianance-app/src/components/layout/BankProgress.js
+++ cob-fianance-app/src/components/layout/BankProgress.js
@@ -7,8 +7,8 @@ export class BankProgress extends Component {
 			<React.Fragment>
 				<ProgressBar style={{fontSize: '1em', height: '1.4em'}} variant="dark" now={100} label={`Total Revenue = $${this.props.revenue}`} />
 				<ProgressBar style={{fontSize: '1em', height: '3em'}}>
-					<ProgressBar variant="blue" now={this.props.square/this.props.revenue*100} key={1} label={`Square = $${this.props.square}`} />
-					<ProgressBar variant="success" now={this.props.bank/this.props.revenue*100} key={2} label={`Bank = $${this.props.bank}`}/>
+					<ProgressBar style={{minWidth: '10em'}} variant="blue" now={this.props.square/this.props.revenue*100} key={1} label={`Square = $${this.props.square}`} />
+					<ProgressBar style={{minWidth: '10rem'}} variant="success" now={this.props.bank/this.props.revenue*100} key={2} label={`Bank = $${this.props.bank}`}/>
 				</ProgressBar>
 			</React.Fragment>
 		)
