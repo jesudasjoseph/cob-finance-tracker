10b6a6f7c8055f306668352549549c6780cbf1b0
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-07-14 07:43:57 -0700

    Added warning Dialog for delete button on form control.

--- cob-fianance-app/src/components/layout/TableControl.js
+++ cob-fianance-app/src/components/layout/TableControl.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 import '../styles/TableControl.css';
 
@@ -13,16 +14,46 @@ Props
 */
 
 export default class TableControl extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			showWarning: false
+		}
+		this.deleteOnClick = this.deleteOnClick.bind(this);
+		this.onConfirm = this.onConfirm.bind(this);
+	}
+
+	deleteOnClick(){
+		this.setState({showWarning: true});
+	}
+
+	onConfirm(response){
+		if (response === 'yes'){
+			this.setState({showWarning: false});
+				this.props.deleteOnClick();
+		}
+		else {
+			this.setState({showWarning: false});
+		}
+	}
+
 	render(){
 		return(
 			<>
-				<div>
-					<div className='flex-container control-container'>
-						{this.props.add && <Button className='addButton' disabled={this.props.addDisabled} variant='success' onClick={this.props.addOnClick}>Add</Button>}
-						{this.props.edit && <Button className='editButton' disabled={this.props.editDisabled} variant='warning' onClick={this.props.editOnClick}>Edit</Button>}
-						{this.props.delete && <Button className='deleteButton' disabled={this.props.deleteDisabled} variant='danger' onClick={this.props.deleteOnClick}>Delete</Button>}
-					</div>
+				<div className='flex-container control-container'>
+					{this.props.add && <Button className='addButton' disabled={this.props.addDisabled} variant='success' onClick={this.props.addOnClick}>Add</Button>}
+					{this.props.edit && <Button className='editButton' disabled={this.props.editDisabled} variant='warning' onClick={this.props.editOnClick}>Edit</Button>}
+					{this.props.delete && <Button className='deleteButton' disabled={this.props.deleteDisabled} variant='danger' onClick={this.deleteOnClick}>Delete</Button>}
 				</div>
+				<Modal show={this.state.showWarning}>
+					<Modal.Body>
+						<p>Are you sure you want to delete this item?</p>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button onClick={()=>this.onConfirm('')} variant="secondary">Cancel</Button>
+						<Button onClick={()=>this.onConfirm('yes')} variant="primary">Delete</Button>
+					</Modal.Footer>
+				</Modal>
 			</>
 		)
 	}
