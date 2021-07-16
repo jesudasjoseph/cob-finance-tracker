990532f57ea145180e4b5a2156f8966f26d3cd2a
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-07-16 05:30:12 -0700

    Added lines to config

--- server/config.js
+++ server/config.js
@@ -5,7 +5,12 @@ dotenv.config();
 module.exports = {
 	porthttps: process.env.PORTHTTPS,
 	porthttp: process.env.PORTHTTP,
+	
 	secret: process.env.API_KEY,
+
 	sslKeyPath: process.env.SSL_KEY_PATH,
-	sslCertPath: process.env.SSL_CERT_PATH
+	sslCertPath: process.env.SSL_CERT_PATH,
+
+	certbotCertificateRenewalURL: process.env.certBotURL,
+	certbotCertificateRenewalMSG: process.env.certBotMSG
 };
