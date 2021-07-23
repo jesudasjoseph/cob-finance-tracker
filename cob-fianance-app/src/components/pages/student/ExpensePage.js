272abfe847a94ece699187e0ad6aec11b838777d
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-07-23 12:17:22 -0700

    added expense search bar

--- cob-fianance-app/src/components/pages/student/ExpensePage.js
+++ cob-fianance-app/src/components/pages/student/ExpensePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Table from 'react-bootstrap/Table';
 import TableControl from '../../layout/TableControl';
 import AddExpenseDialog from '../../layout/AddExpenseDialog';
+import SearchBar from '../../layout/SearchBar';
 import { API_PATH } from '../../Config';
 import './styles/ExpensePage.css';
 
@@ -10,7 +11,8 @@ export default class ExpensePage extends Component {
 		super(props);
 		this.state = {
 			expenseTableData: [],
-			showAddDialog: false
+			showAddDialog: false,
+			searchText: ''
 		}
 		this.fetchExpenseTableData = this.fetchExpenseTableData.bind(this);
 
@@ -18,6 +20,8 @@ export default class ExpensePage extends Component {
 
 		this.addExpenseDialogOnClose = this.addExpenseDialogOnClose.bind(this);
 		this.addExpenseDialogOnSubmit = this.addExpenseDialogOnSubmit.bind(this);
+
+		this.searchOnChange = this.searchOnChange.bind(this);
 	}
 
 	componentDidMount(){
@@ -53,8 +57,8 @@ export default class ExpensePage extends Component {
 		});
 	}
 
-	fetchExpenseTableData(){
-		fetch(API_PATH + '/expense/byuid?start=0&end=50', {
+	fetchExpenseTableData(searchParam){
+		fetch(API_PATH + '/expense/byuid?start=0&end=50' + '&search=' + searchParam, {
 			mode: 'cors',
 			method: 'GET',
 			credentials: 'same-origin',
@@ -68,53 +72,60 @@ export default class ExpensePage extends Component {
 			return response.json();
 		}).then(data => {
 			console.log('Success:', data);
-			if (data != null)
-				this.setState({expenseTableData:data});
+			this.setState({expenseTableData:data});
 		}).catch((error) => {
 			console.error('Error:', error);
 		});
+	}
+
+	searchOnChange(text){
+		this.setState({searchText: text});
+		this.fetchExpenseTableData(text);
 	}
 
 	render() {
 		return (
 			<>
 				<div className='expense-container'>
-					<div className='flex-container left'>
-						<h2>Business Expenses</h2>
-						<Table
-							responsive
-							size="m"
-							striped bordered hover variant="dark">
-							<thead>
-								<tr>
-									<th>Date</th>
-									<th>Product</th>
-									<th>Company</th>
-									<th>Payment Method</th>
-									<th>Quantity</th>
-									<th>Price Per Unit</th>
-									<th>Total</th>
-									<th>Justification</th>
-								</tr>
-							</thead>
-							<tbody>
-								{this.state.expenseTableData.map((expense, index) => {
-									const {quantity,product,company, date, payment_method, price_per_unit, justification, total,eid} = expense;
-									return (
-										<tr key={eid}>
-											<td> {date.split('T')[0]} </td>
-											<td> {product}</td>
-											<td>{company}</td>
-											<td>{payment_method}</td>
-											<td>{quantity}</td>
-											<td>{price_per_unit}</td>
-											<td>{total}</td>
-											<td>{justification}</td>
-										</tr>
-									);
-								})}
-							</tbody>
-						</Table>
+					<div className='left'>
+						<SearchBar onChange={this.searchOnChange}/>
+						<div className='flex-container'>
+							<h2>Business Expenses</h2>
+							<Table
+								responsive
+								size="m"
+								striped bordered hover variant="dark">
+								<thead>
+									<tr>
+										<th>Date</th>
+										<th>Product</th>
+										<th>Company</th>
+										<th>Payment Method</th>
+										<th>Quantity</th>
+										<th>Price Per Unit</th>
+										<th>Total</th>
+										<th>Justification</th>
+									</tr>
+								</thead>
+								<tbody>
+									{this.state.expenseTableData.map((expense, index) => {
+										const {quantity,product,company, date, payment_method, price_per_unit, justification, total,eid} = expense;
+										return (
+											<tr key={eid}>
+												<td> {date.split('T')[0]} </td>
+												<td> {product}</td>
+												<td>{company}</td>
+												<td>{payment_method}</td>
+												<td>{quantity}</td>
+												<td>{price_per_unit}</td>
+												<td>{total}</td>
+												<td>{justification}</td>
+											</tr>
+										);
+									})}
+								</tbody>
+							</Table>
+						</div>
 					</div>
 					<div className='right'>
 						<TableControl add addOnClick={this.addOnClick}/>
