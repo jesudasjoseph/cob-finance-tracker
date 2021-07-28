8cfa94442a3c75b82a8f024aaa37ac56808c3984
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-07-28 16:10:50 -0700

    added q

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authorizor = require('../authorizor');
+const q = require('../queries');
 
 router.get('/reset-code', authorizor.authToken, async (req, res) => {
 	res.setHeader('Content-Type', 'application/json');
