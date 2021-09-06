a2c720e4ff84405327c14c0fe09b4b906c09c759
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-09-05 21:40:31 -0700

    Added toast styled Notifications.

--- cob-fianance-app/src/App.js
+++ cob-fianance-app/src/App.js
@@ -23,6 +23,7 @@ import Page404 from './components/pages/404';
 
 //Notification
 import Notification from './components/layout/Notification';
+import Toast from 'react-bootstrap/Toast';
 
 //Context
 import { AppContext } from './AppContext';
@@ -30,10 +31,20 @@ import { AppContext } from './AppContext';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+function NotificationObj(title, content, type, delay){
+	this.title = title;
+	this.content = content;
+	this.type = type;
+	this.delay = delay;
+	this.show = true;
+}
+
 export default class App extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
+			notificationPool: [],
+			notificationHiddenPoolCount: 0,
 			showNotification: false,
 			notificationType: '',
 			notificationTitle: '',
@@ -46,11 +57,33 @@ export default class App extends Component{
 	}
 
 	pushNotification(type, title, content, timeout){
-		this.setState({notificationType: type, notificationTitle: title, notificationContent: content, notificationTimeout: timeout, showNotification:true});
+		if (this.state.notificationPool.length === 0){
+			this.setState({notificationPool: [new NotificationObj(title, content, type, timeout)]});
+		}
+		else if (this.state.notificationHiddenPoolCount > 0){
+			for (let i = 0; i < this.state.notificationPool.length; i++){
+				if (!this.state.notificationPool[i].show){
+					let tempNotificationPool = this.state.notificationPool;
+					tempNotificationPool[i] = new NotificationObj(title, content, type, timeout);
+					this.setState({notificationPool: tempNotificationPool, notificationHiddenPoolCount: this.state.notificationHiddenPoolCount - 1});
+					break;
+				}
+			}
+		}
+		else {
+			let tempNotificationPool = this.state.notificationPool;
+			tempNotificationPool.push(NotificationObj(title, content, type, timeout));
+			this.setState({notificationPool: tempNotificationPool});
+		}
+		//this.setState({notificationType: type, notificationTitle: title, notificationContent: content, notificationTimeout: timeout, showNotification:true});
 	}
 
-	notificationOnClose(){
-		this.setState({showNotification: false});
+	notificationOnClose(index){
+		let notificationPool = this.state.notificationPool;
+		notificationPool[index].show = false;
+		this.setState({notificationPool: notificationPool, notificationHiddenPoolCount: this.state.notificationHiddenPoolCount + 1});
+
+		//this.setState({showNotification: false});
 	}
 
 	render(){
@@ -97,8 +130,59 @@ export default class App extends Component{
 					</BrowserRouter>
 				</AppContext.Provider>
 
-				<Notification show={this.state.showNotification} type={this.state.notificationType} content={this.state.notificationContent} title={this.state.notificationTitle} onClose={this.notificationOnClose} timeout={this.state.notificationTimeout}/>
+				<div className='toast-container'>
+					{
+						this.state.notificationPool.map((notification, index) => {
+							const { title, content, type, delay, show} = notification;
+							let pool = this.state.notificationPool;
+							return(
+								<Toast className='toast' onClose={() => this.notificationOnClose(index)} show={show} delay={delay} autohide>
+									<Toast.Header>
+										<img
+											src="holder.js/20x20?text=%20"
+											className="rounded me-2"
+											alt=""
+										/>
+										<strong className="me-auto">{title}</strong>
+										<small>{type}</small>
+									</Toast.Header>
+									<Toast.Body>{content}</Toast.Body>
+								</Toast>
+							);
+						})
+					}
+				</div>
+				<div className='toast-container'>
+					<Toast className='toast' show>
+						<Toast.Header>
+							<img
+								src="holder.js/20x20?text=%20"
+								className="rounded me-2"
+								alt=""
+							/>
+							<strong className="mr-auto">Network</strong>
+							<small>Error</small>
+						</Toast.Header>
+						<Toast.Body className='toast-error'>Stuff and things...</Toast.Body>
+					</Toast>
+					<Toast className='toast' show>
+						<Toast.Header>
+							<img
+								src="holder.js/20x20?text=%20"
+								className="rounded me-2"
+								alt=""
+							/>
+							<strong className="mr-auto">Network</strong>
+							<small>Error</small>
+						</Toast.Header>
+						<Toast.Body className='toast-success'>Stuff and things...</Toast.Body>
+					</Toast>
+				</div>
 			</>
 		);
 	}
 }
+
+/*
+<Notification show={this.state.showNotification} type={this.state.notificationType} content={this.state.notificationContent} title={this.state.notificationTitle} onClose={this.notificationOnClose} timeout={this.state.notificationTimeout}/>
+*/
