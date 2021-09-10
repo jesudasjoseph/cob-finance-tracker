aa8c9a4716df64204164684d29e4394228764882
Jesudas Joseph <jesudasmjoseph@gmail.com>
2021-09-09 20:33:17 -0700

    removed missing imports

--- cob-fianance-app/src/components/pages/UnknownONID.js
+++ cob-fianance-app/src/components/pages/UnknownONID.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import { API_PATH } from '../Config';
+
 export default class UnknownONID extends React.Component {
 	render () {
 		return (
